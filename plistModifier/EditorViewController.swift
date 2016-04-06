16b836f838e1d6e6a012a7abfd192d70f5e9d759
huangsuli <sulihuang@sulis-mbp.dynamic.ucsd.edu>
2016-04-06 09:51:36 -0700

    initial commit

---
+++ plistModifier/EditorViewController.swift
@@ -0,0 +1,9 @@
+//
+//  EditorViewController.swift
+//  plistModifier
+//
+//  Created by Suli Huang on 4/2/16.
+//  Copyright © 2016 Suli Huang. All rights reserved.
+//
+
+import Foundation
---
+++ plistModifier/PlistConverter.swift
@@ -0,0 +1,9 @@
+//
+//  PlistConverter.swift
+//  plistModifier
+//
+//  Created by Suli Huang on 4/2/16.
+//  Copyright © 2016 Suli Huang. All rights reserved.
+//
+
+import Foundation
